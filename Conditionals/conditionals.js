864a2ddbbfd0073d897e55e8e6c9e3b8ea3b7434
Benjamin Yoo <byoo0522@gmail.com>
2021-02-11 10:21:28 -0800

    commit on 'master'

---
+++ Conditionals/conditionals.js
@@ -0,0 +1,9 @@
+let number = 11;
+if(number > 0) {
+  //then run the code
+  console.log('hooray');
+  console.log('it is positive');
+} else {
+  console.log("boo!");
+  console.log("negative!");
+}
