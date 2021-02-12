b72c5943dd8e1826d996e31937b4da91d294f063
Benjamin Yoo <byoo0522@gmail.com>
2021-02-11 16:56:07 -0800

    Final

---
+++ Arrays/arrays.js
@@ -0,0 +1,13 @@
+let fruits = ['apple', 'mango', 'kiwi', 'grapes'];
+console.log(fruits);
+console.log(fruits.length); // 4
+console.log(fruits[0]); // apple
+console.log(fruits[1]); // mango
+console.log(fruits[2]); // kiwi
+console.log(fruits.push("cat")); //adds new element to end
+console.log(fruits.push("dog", 'hippo', 'elephant')); //adds new element to the end
+console.log(fruits.pop()); //remove last element
+console.log(fruits.shift()); //removes first element
+console.log(fruits.unshift()); //add new element to beginning
+
+console.log(fruits.indexOf('mango'));
