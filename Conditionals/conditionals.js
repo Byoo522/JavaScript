0c5cf8280058884280f088b408d2dc185daef204
Benjamin Yoo <byoo0522@gmail.com>
2021-02-11 16:56:20 -0800

    final

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -1,9 +1,21 @@
-let number = 11;
+let number = 1;
+
 if(number > 0) {
   //then run the code
   console.log('hooray');
   console.log('it is positive');
-} else {
+} else if (number < 0) {
   console.log("boo!");
   console.log("negative!");
+} else {
+  console.log("it is zero!");
+}
+
+let num = 2;
+if(num > 0) {
+  console.log('it is positive!');
+}
+
+if (number % 2 === 0) {
+  console.log('it is even!');
 }
