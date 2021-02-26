303a63bc402bcfde5a1b416af571360c6bb7d0ce
Benjamin Yoo <byoo0522@gmail.com>
2021-02-25 19:50:50 -0800

    finish add button

--- Udemy JS/variables.data-types.operators.functions/Project 1/scripts/apps.js
+++ Udemy JS/variables.data-types.operators.functions/Project 1/scripts/apps.js
@@ -1,19 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const sumResult = num1 + num2;
-  return sumResult;
+function add() {
+  currentResult = currentResult + parseInt(userInput.value); // parseInt converts test to number with no decimal. parseFloat converts to number with decimals.
+  outputResult(currentResult, '');
 }
 
-add(1, 2);
-add(5, 5);
-add(7, 7);
+addBtn.addEventListener('click', add);
 
-currentResult = currentResult + 10;
-
-let calcualtionDescription = `(${defaultResult} + 10)`;
-let errorMessage = 'An error occured'; // adding a \n will add a line break on the console, but you won't see the line break
-
-
-outputResult(currentResult, calcualtionDescription);
+// currentResult = currentResult + 10;
+// let calcualtionDescription = `(${defaultResult} + 10)`;
+// let errorMessage = 'An error occured'; // adding a \n will add a line break on the console, but you won't see the line break
