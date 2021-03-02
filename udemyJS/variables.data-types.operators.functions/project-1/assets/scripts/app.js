d382caf37263eb4b3942d111a2343949939e092c
Benjamin Yoo <byoo0522@gmail.com>
2021-03-02 09:39:24 -0800

    set up project 2 environment, adjust files and debug

--- udemyJS/variables.data-types.operators.functions/project-1/assets/scripts/app.js
+++ udemyJS/variables.data-types.operators.functions/project-1/assets/scripts/app.js
@@ -2,19 +2,23 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
-
 // Gets input from input field
 function getUserNumberInput() {
-  return parseInt(userInput.value);
+  return parseInt(usrInput.value);
 }
 
 // Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcualtionDescription = `${currentResult} ${operator} ${enteredNumber}`;
-  outputResult(currentResult, calcualtionDescription) //from vendor.js
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
-function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
   const logEntry = {
     operation: operationIdentifier,
     prevResult: prevResult,
@@ -26,43 +30,38 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
 }
 
 function add() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult += enteredNumber; // parseInt converts test to number with no decimal. parseFloat converts to number with decimals.
+  currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
   writeToLog('ADD', initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult -= enteredNumber; // parseInt converts test to number with no decimal. parseFloat converts to number with decimals.
+  currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
   writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult *= enteredNumber; // parseInt converts test to number with no decimal. parseFloat converts to number with decimals.
+  currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
   writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult /= enteredNumber; // parseInt converts test to number with no decimal. parseFloat converts to number with decimals.
+  currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
   writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
-
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
-
-// currentResult = currentResult + 10;
-// let calcualtionDescription = `(${defaultResult} + 10)`;
-// let errorMessage = 'An error occured'; // adding a \n will add a line break on the console, but you won't see the line break
