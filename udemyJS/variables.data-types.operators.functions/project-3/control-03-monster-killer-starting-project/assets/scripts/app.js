d9230f6be496c862a4169bde873e69006e94e368
Benjamin Yoo <byoo0522@gmail.com>
2021-03-02 13:03:16 -0800

    add strong attack button

--- udemyJS/variables.data-types.operators.functions/project-3/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ udemyJS/variables.data-types.operators.functions/project-3/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10; //maximum dmg
+const STRONG_ATTACK_VALUE = 17; // maximum dmg
 const MONSTER_ATTACK_VALUE = 14; //maximum dmg
+
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -12,11 +14,28 @@ function attackHandler() {
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
-  if(currentMonsterHealth <= 0) {
+  if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You win!');
-  } else if (currentPlayerHealth <= 0) {
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lose!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
+}
+
+function StrongAttackHandler() {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You win!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lose!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
   }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', StrongAttackHandler)
