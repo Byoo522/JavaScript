e5d89db7ff9e5bd908d661d9fc14d4802817003c
Benjamin Yoo <byoo0522@gmail.com>
2021-03-02 12:56:53 -0800

    add monster damage to player button

--- udemyJS/variables.data-types.operators.functions/project-3/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ udemyJS/variables.data-types.operators.functions/project-3/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
-const ATTACK_VALUE = 10;
+const ATTACK_VALUE = 10; //maximum dmg
+const MONSTER_ATTACK_VALUE = 14; //maximum dmg
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,13 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if(currentMonsterHealth <= 0) {
+    alert('You win!');
+  } else if (currentPlayerHealth <= 0) {
+    alert('You lose!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
